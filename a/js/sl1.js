40cd509e147f1cb3a774e43b3d8aa0d8b74d348e
wangshangyonghu <104623063+wangshangyonghu@users.noreply.github.com>
2026-05-23 14:01:28 +0800

    Add files via upload

---
+++ a/js/sl1.js
@@ -0,0 +1,13 @@
+
+// 今日头条自动推送脚本
+//  作用：让页面自动提交收录
+// wangshangyonghu.github.io
+
+(function(){
+var el = document.createElement("script");
+el.src = "https://lf1-cdn-tos.bytegoofy.com/goofy/ttzz/push.js?4fe1616d6b94069ca37b85d5963fbf3e9b5e8f8ec37dc597cbf0a0f9432db085ed00990171a01a724f0f07b37598cb604a4d40de0dd5fa9b5c2c10f69a3b501dc430e831103b45ce33654fb9f95b006c";
+el.id = "ttzz";
+var s = document.getElementsByTagName("script")[0];
+s.parentNode.insertBefore(el, s);
+})(window)
+
