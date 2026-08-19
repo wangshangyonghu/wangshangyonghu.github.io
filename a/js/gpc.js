110e989ac73c782a73fa02b0f394d734959cda4d
wangshangyonghu <104623063+wangshangyonghu@users.noreply.github.com>
2026-08-19 09:57:37 +0800

    Add files via upload

---
+++ a/js/gpc.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '/a/js/gpc.html', true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      document.body.insertAdjacentHTML('beforeend', xhr.responseText);
+    }
+  };
+  xhr.send();
+});
